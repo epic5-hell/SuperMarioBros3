262dabd3fbd07d7c10abbbd7142c8534abc2ad43
= <=>
2020-12-01 11:15:10 +0700

    update goomba

--- SuperMarioBros3/Goomba.cpp
+++ SuperMarioBros3/Goomba.cpp
@@ -1,8 +1,8 @@
 #include "Goomba.h"
 
-CGoomba::CGoomba(int _type_goomba)
+CGoomba::CGoomba(int goomba_type)
 {
-	type_goomba = _type_goomba;
+	type = goomba_type;
 	SetState(GOOMBA_STATE_WALKING);
 }
 void CGoomba::CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents)
@@ -19,25 +19,73 @@ void CGoomba::CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LP
 
 	std::sort(coEvents.begin(), coEvents.end(), CCollisionEvent::compare);
 }
-void CGoomba::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+void CGoomba::FilterCollision(vector<LPCOLLISIONEVENT>& coEvents, vector<LPCOLLISIONEVENT>& coEventsResult, float& min_tx, float& min_ty, float& nx, float& ny, float& rdx, float& rdy)
 {
-	if (state == GOOMBA_STATE_DIE_BY_KICK || state == GOOMBA_STATE_DIE)
+	min_tx = 1.0f;
+	min_ty = 1.0f;
+	int min_ix = -1;
+	int min_iy = -1;
+
+	nx = 0.0f;
+	ny = 0.0f;
+
+	coEventsResult.clear();
+
+	for (UINT i = 0; i < coEvents.size(); i++)
 	{
-		left = top = right = bottom = 0;
-		return;
+		LPCOLLISIONEVENT c = coEvents[i];
+
+		if (c->t < min_tx && c->nx != 0) {
+			min_tx = c->t; nx = c->nx; min_ix = i; rdx = c->dx;
+		}
+
+		if (c->t < min_ty && c->ny != 0) {
+			min_ty = c->t; ny = c->ny; min_iy = i; rdy = c->dy;
+		}
+
+		if (dynamic_cast<CMario*>(c->obj))
+		{
+			ny = -0.001f;
+		}
 	}
 
+	if (min_ix >= 0) coEventsResult.push_back(coEvents[min_ix]);
+	if (min_iy >= 0) coEventsResult.push_back(coEvents[min_iy]);
+}
+
+void CGoomba::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
 	left = x;
 	top = y;
-	right = x + GOOMBA_NORMAL_BBOX_WIDTH;
 
 	if (state == GOOMBA_STATE_DIE)
 	{
-		bottom = y + GOOMBA_BBOX_HEIGHT_DIE;
+		top = y + 7;
+		right = left + GOOMBA_NORMAL_BBOX_WIDTH;
+		bottom = y + GOOMBA_NORMAL_BBOX_HEIGHT;
+	}
+	else if (state == GOOMBA_STATE_WALKING)
+	{
+		if (type == GOOMBA_TYPE_NORMAL)
+		{
+			right = x + GOOMBA_NORMAL_BBOX_WIDTH;
+			bottom = y + GOOMBA_NORMAL_BBOX_HEIGHT;
+		}
+		else
+		{
+			right = x + GOOMBA_WINGS_BBOX_WIDTH;
+			bottom = y + GOOMBA_WINGS_BBOX_HEIGHT;
+		}
+	}
+	else if (state == GOOMBA_STATE_LOSE_WINGS)
+	{
+		top = y + 7;
+		right = x + GOOMBA_LOSE_WINGS_BBOX_WIDTH;
+		bottom = y + GOOMBA_NORMAL_BBOX_HEIGHT;
 	}
 	else
 	{
-		bottom = y + GOOMBA_NORMAL_BBOX_HEIGHT;
+		left = top = right = bottom = 0;
 	}
 }
 
@@ -58,8 +106,20 @@ void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 	coEvents.clear();
 
 	// turn off collision when goomba died
-	if (state != GOOMBA_STATE_DIE_BY_KICK || state != GOOMBA_STATE_DIE)
+	if (state != GOOMBA_STATE_DIE_BY_KICK || state != GOOMBA_STATE_DISAPPEAR)
 		CalcPotentialCollisions(coObjects, coEvents);
+
+	if (state == GOOMBA_STATE_DIE)
+	{
+		if (dying_start == 0)
+		{
+			StartDying();
+		}
+		if (GetTickCount64() - dying_start >= 200)
+		{
+			SetState(GOOMBA_STATE_DISAPPEAR);
+		}
+	}
 
 	// No collision occured, proceed normally
 	if (coEvents.size() == 0)
@@ -80,8 +140,11 @@ void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
 
 		// groomba walks on block 
-		x += min_tx * dx + nx * 0.4f;		// nx*0.4f : need to push out a bit to avoid overlapping next frame
-		y += min_ty * dy + ny * 0.4f;
+		if (state != GOOMBA_STATE_DIE)
+		{
+			x += min_tx * dx + nx * 0.4f;		// nx*0.4f : need to push out a bit to avoid overlapping next frame
+			y += min_ty * dy + ny * 0.4f;
+		}
 
 		if (ny != 0) vy = 0;
 
@@ -104,35 +167,31 @@ void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 			}
 			else // Collisions with the others
 			{
-				if (nx != 0 && ny == 0)
+				if (nx != 0 && ny == 0 && !dynamic_cast<CKoopas*>(e->obj))
 				{
 					vx = -vx;
 				}
 			}
 		}
 	}
-	//if (vx < 0 && x < 0) 
-	//{
-	//	
-	//	vx = -vx;
-	//}
 	// clean up collision events
-	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+	for (UINT i = 0; i < coEvents.size(); i++) 
+		delete coEvents[i];
 }
 
 void CGoomba::Render()
 {
 	int ani = -1;
-	switch (type_goomba)
+	switch (type)
 	{
 	case GOOMBA_TYPE_NORMAL:
 	{
 		ani = GOOMBA_NORMAL_ANI_WALKING;
-		if (state == GOOMBA_STATE_DISAPPEAR) return;
+		if (state == GOOMBA_STATE_DISAPPEAR)
+			return;
 		else if (state == GOOMBA_STATE_DIE)
 		{
 			ani = GOOMBA_NORMAL_ANI_DIE;
-			state = GOOMBA_STATE_DISAPPEAR;
 		}
 		else if (state == GOOMBA_STATE_DIE_BY_KICK)
 		{
@@ -140,9 +199,10 @@ void CGoomba::Render()
 		}
 		break;
 	}
-	case GOOMBA_TYPE_WINGS: return;
+	case GOOMBA_TYPE_WINGS:
+		return;
+		break;
 	}
-
 	animation_set->at(ani)->Render(x, y);
 
 	//RenderBoundingBox();
@@ -154,13 +214,15 @@ void CGoomba::SetState(int state)
 	switch (state)
 	{
 	case GOOMBA_STATE_DIE:
-		y += GOOMBA_NORMAL_BBOX_HEIGHT - GOOMBA_BBOX_HEIGHT_DIE + 10;
 		vx = 0;
 		vy = 0;
 		break;
 	case GOOMBA_STATE_DIE_BY_KICK:
 		vy = -GOOMBA_DIE_DEFLECT_SPEED;
-		//vx = -vx;
+		vx = -vx;
+		break;
+	case GOOMBA_STATE_LOSE_WINGS:
+		vy = 0;
 		break;
 	case GOOMBA_STATE_WALKING:
 		vx = -GOOMBA_WALKING_SPEED;
--- SuperMarioBros3/Goomba.h
+++ SuperMarioBros3/Goomba.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "GameObject.h"
 #include "algorithm"
+#include "Mario.h"
+#include "Koopas.h"
 
 #define GOOMBA_WALKING_SPEED		0.05f
 #define GOOMBA_GRAVITY				0.002f
@@ -11,13 +13,15 @@
 #define GOOMBA_NORMAL_BBOX_HEIGHT	15
 #define GOOMBA_WINGS_BBOX_WIDTH		20
 #define GOOMBA_WINGS_BBOX_HEIGHT	24
-#define GOOMBA_GOOMBA_LOSE_WINGS_BBOX_HEIGHT	19
-#define GOOMBA_BBOX_HEIGHT_DIE		9
+#define GOOMBA_LOSE_WINGS_BBOX_WIDTH	20
+#define GOOMBA_LOSE_WINGS_BBOX_HEIGHT	19
+
 
 #define GOOMBA_STATE_WALKING		100
 #define GOOMBA_STATE_DIE			200
 #define GOOMBA_STATE_DISAPPEAR		300
 #define GOOMBA_STATE_DIE_BY_KICK	400
+#define GOOMBA_STATE_LOSE_WINGS		500
 
 #define GOOMBA_NORMAL_ANI_WALKING	0
 #define GOOMBA_NORMAL_ANI_DIE		1
@@ -27,19 +31,21 @@
 
 class CGoomba : public CGameObject
 {
-
-	int type_goomba;
-
+	int type;
+	DWORD dying_start = 0;
 
 public:
+	CGoomba(int goomba_type);
+	void CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents);
+	void FilterCollision(vector<LPCOLLISIONEVENT>& coEvents, vector<LPCOLLISIONEVENT>& coEventsResult, float& min_tx, float& min_ty, float& nx, float& ny, float& rdx, float& rdy);
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
-	void CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents);
-	CGoomba(int _type_goomba);
+	
+	void StartDying() { dying_start = GetTickCount64(); }
 	int GetType()
 	{
-		return type_goomba;
+		return type;
 	}
 	virtual void SetState(int state);
 };
